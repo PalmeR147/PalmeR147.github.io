bf9df084f639504ecc635bb873a61eb8c9e1caac
Marcus <Palmr>
2017-03-25 12:48:23 +0100

    scriptJs

--- script.js
+++ script.js
@@ -1,50 +1,3 @@
-<<<<<<< HEAD
 $( document ).ready(function() {	$("#test").css("color","blue");	var wowDirectory = "http://render-eu.worldofwarcraft.com/character/";		var repStages = ["Hated","Hostile","Unfriendly","Neutral","Friendly","Honored","Revered","Exalted"]		//When "Go!" button is clicked, initiate API CALL	$("#search").click(apiCall);	function apiCall(){		var apiURL = getApiURL($("#charName").val(), $("#realmName").val(), "reputation");		console.log(apiURL);				//BeginJSONCode		$.getJSON(apiURL, function(data){			console.log(data);			$("#test").html(data.thumbnail);			$("#avatar").attr("src", wowDirectory + data.thumbnail);			$.each(data.reputation, function(arrayID,faction) {
 			    //$("#testDiv").append('<p class="factionName">' + faction.name + '</p>');				if(faction.max != 8400)					$("#testDiv").append('<div class="factionBox"><h1 class="factionName">'+faction.name+'</h1><h2 class="factionStanding">'+repStages[faction.standing]+'</h2><div class="reputationProgress"><p class="repCurrent">'+faction.value+'</p><p class="repMax">/'+faction.max+'</p></div></div>');
 			});		});		//EndJSONCode	}		//Function to format the API String to match current character + realm + field	function getApiURL(name, realm, field)	{		var completeURL = "https://eu.api.battle.net/wow/character/" + realm + "/" + name + "?fields=" + field + "&locale=en_GB&apikey=8cnx86bfjey75u5rjryrhc37p2s9d82t";		return completeURL;	}});
-=======
-$( document ).ready(function() {
-
-
-
-	$("#test").css("color","blue");
-
-	var wowDirectory = "https://render-eu.worldofwarcraft.com/character/";
-	
-	//When "Go!" button is clicked, initiate API CALL
-	$("#search").click(apiCall);
-
-	function apiCall(){
-		var apiURL = getApiURL($("#charName").val(), $("#realmName").val(), "reputation");
-		console.log(apiURL);
-		
-		//BeginJSONCode
-		$.getJSON(apiURL, function(data){
-			console.log(data);
-			$("#test").html(data.thumbnail);
-			$("#avatar").attr("src", wowDirectory + data.thumbnail);
-
-			$("#json").text(data.reputation[1].name);
-
-			/*$.each(data.reputation,(function () {
-			    $("#json").append(this.name);
-			});*/
-
-			$.each(data.reputation, function(arrayID,faction) {
-			    $("#testDiv").append('<p class="factionName">' + faction.name + '</p>');
-			});
-		});
-		//EndJSONCode
-	}
-	
-	//Function to format the API String to match current character + realm + field
-	function getApiURL(name, realm, field)
-	{
-		var completeURL = "https://eu.api.battle.net/wow/character/" + realm + "/" + name + "?fields=" + field + "&locale=en_GB&apikey=8cnx86bfjey75u5rjryrhc37p2s9d82t";
-		return completeURL;
-	}
-
-
-
-});
->>>>>>> origin/master
